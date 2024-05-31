e6d98e2058ca8b1be3de620ca3eb8245132da343
Duwayne Frieslaar <155962320+DFreezy@users.noreply.github.com>
2024-05-31 11:16:04 +0200

    Added completed files
    I added all files

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Header from "./component/Header"
+import Meme from "./component/Meme"
+/**
+ * Challenge: Build the Header component
+ */
+export default function App() {
+    return (
+        <div>
+            <Header />
+            <Meme/>
+        </div>
+    )
+}
---
+++ component/Header.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Header() {
+    return (
+        <header className="header">
+        <img src="https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?cs=srgb&dl=pexels-pixabay-45201.jpg&fm=jpg" className="picture"/>
+        <h2 className="title">Meme generator</h2>
+        <h4 className="project">React course-3</h4>
+        </header>
+    )
+}
